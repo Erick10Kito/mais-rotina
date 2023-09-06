6986c8fe694c40e43c579e1ea9dbff8206867086
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-09-06 11:05:17 -0300

    Firebase files separated into a specific file

---
+++ src/config/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore";
+
+const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_ID,
+});
+export const keyTask = "tasks";
+
+export const db = getFirestore(firebaseApp);
+
+export const taskCollectionRef = collection(db, keyTask);
