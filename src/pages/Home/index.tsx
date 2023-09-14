b4f8405ca2cd57f1155c843645b327226d227344
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-09-14 17:38:47 -0300

    Log out button created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,25 @@
 import { useContext, useEffect, useState } from "react";
 
 import { collection, doc, onSnapshot } from "firebase/firestore";
-import { db } from "../../config/firebase/firebase";
+import { auth, db } from "../../config/firebase/firebase";
 import { Header } from "../../components/Header";
 import { AddBar } from "../../components/AddBar";
 import { Dashboard } from "../../components/Dashboard";
 import { ITarefa } from "../../types/todo.ds";
 import { keyTask, keyUserTasks } from "../../config/firebase/keys";
 import { Context } from "../../context/AuthContext";
+import { SignOut } from "@phosphor-icons/react";
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
   const { user } = useContext(Context);
   const [tasks, setTasks] = useState<ITarefa[]>([]);
+  const navigate = useNavigate();
+  function handleSignOut() {
+    auth.signOut().then(() => {
+      navigate("/");
+    });
+  }
 
   //console.log(tarefas);
 
@@ -31,9 +39,20 @@ export function Home() {
   }, []);
 
   return (
-    <div className={`${tasks.length <= 2 && "overflow-y-hidden max-h-screen"}`}>
+    <div
+      className={`${
+        tasks.length <= 2 && "overflow-y-hidden max-h-screen"
+      } relative`}
+    >
+      <button
+        title="Deslogar"
+        onClick={handleSignOut}
+        className="text-white text-sm font-normal bg-[#1E6F9F] flex p-3 items-center rounded-full hover:opacity-75 fixed right-3 bottom-3 max-[479px]:p-2"
+      >
+        <SignOut size={20} color="white" weight="bold" />
+      </button>
       <Header />
-      <h1></h1>
+
       <div className="flex justify-center bg-[#1A1A1A]  px-4">
         <div
           className={`max-w-7xl w-full h-screen pb-10 ${
